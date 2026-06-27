dbc6fb2a78063b5bd59a7ef2e2642f539e2c6775
dinesh <dineshbhagatbpl@gmail.com>
2026-06-27 17:48:26 +0530

    adjust css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -116,7 +116,7 @@ export class AppComponent {
         setTimeout(() => {
           splash?.remove();
         }, 500);
-      }, 2500); // 2.5 sec
+      }, 2000); // 2 sec
     });
 
     // const alreadyShown = sessionStorage.getItem('splashShown');
